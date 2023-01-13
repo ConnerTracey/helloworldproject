a1eebef2c4cf8470841d30347eaa1a48a60708d7
ConnerTracey <122555182+ConnerTracey@users.noreply.github.com>
2023-01-13 12:06:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("HELLO")
+    basic.showString("START")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
