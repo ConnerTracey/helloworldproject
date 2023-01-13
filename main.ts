18743f674d5c0657785cdf5856b6ea68ac13e380
ConnerTracey <122555182+ConnerTracey@users.noreply.github.com>
2023-01-13 12:25:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("STARTING")
 })
 input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
     basic.showNumber(1)
     for (let index = 0; index < 1; index++) {
         music.playTone(349, music.beat(BeatFraction.Whole))
     }
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("B E B F C D C5 F ", 130)
 })
 basic.showString("PRESS A")
 for (let index = 0; index < 2; index++) {
