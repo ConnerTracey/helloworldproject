5575ced568095e8f32a448d00c27b964fe07e9b5
ConnerTracey <122555182+ConnerTracey@users.noreply.github.com>
2023-01-13 12:22:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . # . .
-        . # . # .
-        . # # # .
-        # . . . #
-        # . . . #
-        `)
+    basic.showString("STARTING")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("START")
+	
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # # # .
-        . # . # .
-        . # # . .
-        `)
+    basic.showNumber(1)
+    for (let index = 0; index < 1; index++) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    }
 })
-basic.showLeds(`
-    . . # . .
-    # . # . #
-    . # # # .
-    # . # . #
-    # . # . #
-    `)
+basic.showString("PRESS A")
+for (let index = 0; index < 2; index++) {
+    music.playMelody("E B C5 A B G A F ", 130)
+}
+control.waitMicros(15)
+basic.showString("FINISH")
